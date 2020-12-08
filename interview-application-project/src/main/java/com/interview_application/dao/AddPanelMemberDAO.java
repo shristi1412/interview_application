8d2ab4fbb6e9b3a5ea7fe5a4554721ade64c8713
Sneha Chitale <Sneha Chitale@DESKTOP-2AKDBC1>
2020-12-08 09:53:09 +0530

    ViewCandidateforHR added

---
+++ interview-application-project/src/main/java/com/interview_application/dao/AddPanelMemberDAO.java
@@ -0,0 +1,7 @@
+package com.interview_application.dao;
+
+import com.interview_application.entity.PanelMemberEntity;
+
+public class AddPanelMemberDAO {
+	PanelMemberEntity p = new PanelMemberEntity();
+}
