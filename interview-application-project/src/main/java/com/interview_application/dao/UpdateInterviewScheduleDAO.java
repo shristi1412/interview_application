845594704331f57ba1d609886c2285e69478ccf6
Suraj <mandal.suraj.16it1087@gmail.com>
2020-12-08 14:58:23 +0530

    update Interview dao

--- interview-application-project/src/main/java/com/interview_application/dao/UpdateInterviewScheduleDAO.java
+++ interview-application-project/src/main/java/com/interview_application/dao/UpdateInterviewScheduleDAO.java
@@ -1,6 +1,11 @@
 package com.interview_application.dao;
+import com.interview_application.entity.InterviewSchedulerEntity;
+import com.interview_application.exception.InterviewNotFoundException;
+import com.interview_application.exception.RatingNotFoundException;
 
 public interface UpdateInterviewScheduleDAO {
-	InterviewIDEntity findById(String interviewID) throws InterviewNotFoundException;
+	InterviewSchedulerEntity findById(String interviewID) throws InterviewNotFoundException;
+	
+	InterviewSchedulerEntity findById(int techrating, int hrrating) throws RatingNotFoundException;
 
 }
--- interview-application-project/src/main/java/com/interview_application/dao/UpdateInterviewScheduleDAOImpl.java
+++ interview-application-project/src/main/java/com/interview_application/dao/UpdateInterviewScheduleDAOImpl.java
@@ -7,6 +7,10 @@ import javax.persistence.Persistence;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.interview_application.entity.InterviewSchedulerEntity;
+import com.interview_application.exception.InterviewNotFoundException;
+import com.interview_application.exception.RatingNotFoundException;
+
 public class UpdateInterviewScheduleDAOImpl implements UpdateInterviewScheduleDAO{
 	
 	private static Logger logger = LogManager.getLogger(UpdateInterviewScheduleDAOImpl.class.getName());	
@@ -17,11 +21,20 @@ public class UpdateInterviewScheduleDAOImpl implements UpdateInterviewScheduleDA
 		entityManager = entityManagerFactory.createEntityManager();
 	}
 	
-	public InterviewIDEntity findById(String interviewId) throws InterviewNotFoundException {
-		InterviewIDEntity itemEntity = entityManager.find(ItemEntity.class, itemId);
-		logger.info("Database returned ItemEntity: " + itemEntity);
-		if(itemEntity==null)
-			throw new InterviewNotFoundException("ItemId: " + itemId);
-		return itemEntity;
+	public InterviewSchedulerEntity findById(String interviewId) throws InterviewNotFoundException {
+		InterviewSchedulerEntity interviewSchedulerEntity = entityManager.find(InterviewSchedulerEntity.class, interviewId);
+		logger.info("Database returned ItemEntity: " + interviewSchedulerEntity);
+		if(interviewSchedulerEntity==null)
+			throw new InterviewNotFoundException("ItemId: " + interviewId);
+		return interviewSchedulerEntity;
+	}
+		
+		
+		public InterviewSchedulerEntity findById(int techrating, int hrrating) throws RatingNotFoundException {
+			InterviewSchedulerEntity interviewSchedulerEntity1 = entityManager.find(InterviewSchedulerEntity.class, techrating);
+			logger.info("Database returned ItemEntity: " + interviewSchedulerEntity1);
+			if(interviewSchedulerEntity1==null)
+				throw new RatingNotFoundException("ItemId: " + techrating);
+			return interviewSchedulerEntity1;
 	}
 }
