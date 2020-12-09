49457117b72788a78576eee22843c51c6c28125d
shristi1412 <48906851+shristi1412@users.noreply.github.com>
2020-12-09 10:16:39 +0530

    View HR

--- interview-application-project/src/main/java/com/interview_application/service/ViewInterviewMembersServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.interview_application.service;
-
-public class ViewInterviewSchedulerServiceImpl {
-
-	public ViewInterviewSchedulerServiceImpl() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
