6bedc85fdc20cc78ba075e0054012f25e9856e33
Sneha Chitale <snehachitale26@gmail.com>
2020-12-09 10:23:20 +0530

    Surrender as HR

--- interview-application-project/src/main/java/com/interview_application/service/SurrenderAsHRPanelService.java
+++ interview-application-project/src/main/java/com/interview_application/service/SurrenderAsHRPanelService.java
@@ -1,10 +1,11 @@
 package com.interview_application.service;
 
+import com.interview_application.dto.EmployeeDTO;
 import com.interview_application.dto.PanelMemberDTO;
 import com.interview_application.exception.EmployeeNotFoundException;
 
 public interface SurrenderAsHRPanelService {
 	
-	PanelMemberDTO searchById(String employeeID) throws EmployeeNotFoundException;
+	EmployeeDTO searchById(String employeeID) throws EmployeeNotFoundException;
 
 }
